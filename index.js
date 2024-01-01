46aeb94c5fdd83eb3bcbb7b68e41feae5507b115
lifaet <lifaethossain@gmail.com>
2024-01-01 15:45:00 +0600

    Update index.js

--- index.js
+++ index.js
@@ -20688,7 +20688,7 @@
             description: 'My very first portfolio project with a little help from some open source project on github',
             image: "assets/projects/pf.jpg",
             tags: ["webgl", "javascript", "html", "css"],
-            twitter: "https://twitter.com/lifaeth",
+            twitter: "https://facebook.com/lifaet",
             alt: "My very first portfolio project",
             bannerIcons: [{
                 src: "assets/cssda-icon.png",
@@ -20726,7 +20726,11 @@
             }
             renderButtons(e) {
                 let t = "";
-                return t = e.github ? `\n                <div id="work-item-gray-button-${e.id}" class="work-item-gray-button center gray-hover" ${e.liveview?"":'style="width: 100%"'}>\n                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"  class="code-icon">\n                        <use href="#code-path"/>\n                    </svg>\n                 ${e.liveview?"":"<span>Source Code</span>"}\n                </div>\n                 ${e.liveview?`<div id="work-item-orange-button-${e.id}" class="work-item-orange-button small-button center orange-hover">Live View</div>`:""}\n            ` : e.twitter ? `\n            <div id="work-item-orange-button-${e.id}" class="work-item-orange-button small-button center orange-hover" style="width: 100%; margin: 0;">\n                <svg fill="#ffffff" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="24px" height="24px" style="margin-right: 5px">    \n                    <path d="M28,6.937c-0.957,0.425-1.985,0.711-3.064,0.84c1.102-0.66,1.947-1.705,2.345-2.951c-1.03,0.611-2.172,1.055-3.388,1.295 c-0.973-1.037-2.359-1.685-3.893-1.685c-2.946,0-5.334,2.389-5.334,5.334c0,0.418,0.048,0.826,0.138,1.215 c-4.433-0.222-8.363-2.346-10.995-5.574C3.351,6.199,3.088,7.115,3.088,8.094c0,1.85,0.941,3.483,2.372,4.439 c-0.874-0.028-1.697-0.268-2.416-0.667c0,0.023,0,0.044,0,0.067c0,2.585,1.838,4.741,4.279,5.23 c-0.447,0.122-0.919,0.187-1.406,0.187c-0.343,0-0.678-0.034-1.003-0.095c0.679,2.119,2.649,3.662,4.983,3.705 c-1.825,1.431-4.125,2.284-6.625,2.284c-0.43,0-0.855-0.025-1.273-0.075c2.361,1.513,5.164,2.396,8.177,2.396 c9.812,0,15.176-8.128,15.176-15.177c0-0.231-0.005-0.461-0.015-0.69C26.38,8.945,27.285,8.006,28,6.937z"/>\n                </svg>\n                Stay up to date\n            </div>` : `\n                <div id="work-item-gray-button-${e.id}" class="work-item-gray-button center" style="width: 100%; background: #a7adb8; cursor: unset;">\n                    Work in progress\n                </div>\n            `, t
+                return t = e.github ? `\n                <div id="work-item-gray-button-${e.id}" class="work-item-gray-button center gray-hover" ${e.liveview?"":'style="width: 100%"'}>\n                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"  class="code-icon">\n                        <use href="#code-path"/>\n                    </svg>\n                 ${e.liveview?"":"<span>Source Code</span>"}\n                </div>\n                 ${e.liveview?`<div id="work-item-orange-button-${e.id}" class="work-item-orange-button small-button center orange-hover">Live View</div>`:""}\n            ` : e.twitter ? `\n            <div id="work-item-orange-button-${e.id}" class="work-item-orange-button small-button center orange-hover" style="width: 100%; margin: 0;">\n    <svg class="social-icon"
+                xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24">
+                <path fill="currentColor"
+                    d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95z" />
+            </svg> \n                 Stay up to date\n            </div>` : `\n                <div id="work-item-gray-button-${e.id}" class="work-item-gray-button center" style="width: 100%; background: #a7adb8; cursor: unset;">\n                    Work in progress\n                </div>\n            `, t
             }
             renderTags(e) {
                 let t = "";
